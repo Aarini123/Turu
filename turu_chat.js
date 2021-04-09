7c9217b98c70533cab5b35daf907dc1245386ba6
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-04-09 16:55:26 +0530

    Add files via upload

--- turu_chat.js
+++ turu_chat.js
@@ -14,9 +14,28 @@ var firebaseConfig = {
 function getData() { firebase.database().ref("/"+roomName).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
        firebase_message_id = childKey;
        message_data = childData;
-//Start code
-
-//End code
+       firebase.database().ref(room_name).push({
+        name:userName,
+        msg:message,
+        like:0
+  });
+  document.getElementById("chat_input").value="";
+  }
+  function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+           firebase_message_id = childKey;
+           message_data = childData;
+  //Start code
+  console.log(firebase_message_id);
+  console.log(message_data);
+  name1=message_data['name'];
+  message=message_data['msg'];
+  like=message_data['like'];
+  name_tag="<h4>"+ name1+"<img class='user_tick' src='tick.png'></h4>";
+  message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+  like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+           span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+   row=name_tag+message_with_tag+like_button+span_with_tag;
+   document.getElementById("output").innerHTML+=row;
     } });  }); }
 getData();
 function LogOut(){
